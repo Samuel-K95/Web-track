176790bea7d5a0505f8f34007d695409e72dd7ac
Samuel Keno <sgere195@gmail.com>
2024-08-01 09:22:53 +0300

    tutorial

---
+++ todo-ts-react/src/components/TodoForm.tsx
@@ -0,0 +1,33 @@
+import React, { Dispatch, ReactNode, SetStateAction, useState } from "react";
+import TodoService from "../TodoService";
+import TodoTypes from "../todo";
+import "../CSS/TodoForm.css";
+
+interface PropTypes {
+  setTodos: Dispatch<SetStateAction<TodoTypes[]>>;
+}
+
+const TodoForm: React.FC<PropTypes> = ({ setTodos }) => {
+  const [newTodoText, setNewTodoText] = useState<string>("");
+  const handleAddTodo = () => {
+    if (newTodoText.trim() !== "") {
+      const newTodo = TodoService.addTodo(newTodoText);
+      setTodos((prevTodo) => [...prevTodo, newTodo]);
+      setNewTodoText("");
+    }
+  };
+  return (
+    <div className="inputForm">
+      <input
+        type="text"
+        value={newTodoText}
+        onChange={(e) => setNewTodoText(e.target.value)}
+        autoFocus={true}
+        placeholder="Add a task"
+      />
+      <button onClick={handleAddTodo}>Add Todo</button>
+    </div>
+  );
+};
+
+export default TodoForm;
